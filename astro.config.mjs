d3648424591c7a948fd44e89aefe17c1775810d0
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-06-29 04:13:03 +0700

    remove : vercel code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
-
-const isVercelBuild = process.env.VERCEL === '1' || process.env.ASTRO_ADAPTER === 'vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: isVercelBuild ? vercel() : node({ mode: 'standalone' }),
+  adapter: node({ mode: 'standalone' }),
   integrations: [react()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
